e294170f195bf08b623f10f9e8fd22c6340d963b
ssx <s95175379@gmail.com>
2021-12-09 21:30:59 +0800

    first

---
+++ test_p41/src/test_p41.java
@@ -0,0 +1,34 @@
+
+public class test_p41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car(1234,20.5);
+		car1.vshow();
+		car1.mshow();
+	}
+
+}
+class Car implements iVehicle,iMaterial{
+	private int num;
+	private double gas;
+	
+	public Car(int n,double g) {
+		num = n;
+		gas = g;
+		System.out.println("生產了車號為"+ num +"汽油量為"+ gas +"的車子");
+	}
+	public void vshow() {
+		System.out.println("車號是"+ num);
+		System.out.println("汽油量是"+ gas);
+	}
+	public void mshow() {
+		System.out.println("車子的材質是鐵");
+	}
+}
+interface iVehicle{
+	void vshow();
+}
+interface iMaterial{
+	void mshow();
+}
